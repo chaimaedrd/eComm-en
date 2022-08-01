ed1657d527ac961f01882a99da89b8016a558df2
chaimae <dardourichaimae0@gmail.com>
2022-08-01 20:13:40 +0100

    logout + sessions

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,10 +1,14 @@
  <?php
-  use App\Http\Controllers\ProductController;
+ use App\Http\Controllers\ProductController;
+ $total=0;
+ if(Session::has('user')){
   $total=ProductController::cartItem();
+ }
+  
 ?> 
 
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
+  <a class="navbar-brand" href="/">E-Comm</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -12,7 +16,7 @@
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav mr-auto">
       <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+        <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="#">Orders</a>
@@ -23,10 +27,46 @@
       <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
-    <li class="nav-item">
+      <li class="nav-item">
         <a class="nav-link" href="#">Cart({{$total}})</a>
       </li>
+
+      <!-- The dropdown doesnt work -->
+
+      <!-- <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown</a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="#one">one</a>
+          <a class="dropdown-item" href="#two">two</a>
+          <div role="separator" class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#three">three</a>
+        </div>
+      </li> -->
+
+<!-- Second exemple -->
+
+      <!-- <div class="dropdown">
+  <button class="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-expanded="false">
+    Dropdown button
+  </button>
+  <div class="dropdown-menu">
+    <a class="dropdown-item" href="#">Action</a>
+    <a class="dropdown-item" href="#">Another action</a>
+    <a class="dropdown-item" href="#">Something else here</a>
+  </div>
+</div> -->
+      @if(Session::has('user'))
+        <li class="nav-item">
+          <a class="nav-link" href="/logout">Logout</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#">{{Session::get('user')['name']}}</a>
+        </li>
+      @else
+        <li class="nav-item">
+          <a class="nav-link" href="/login">Login</a>
+        </li>
+      @endif
     </ul>
-    
   </div>
 </nav>
