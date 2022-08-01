74125f222288c5032d4434735f7d4c9e41f41b70
chaimae <dardourichaimae0@gmail.com>
2022-08-01 18:59:13 +0100

    complete the Add_To_Cart

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Product;
 use App\Models\Cart;
+use Session; // to get the data from the session in the function: cartItem
 
 class ProductController extends Controller
 {
@@ -29,5 +30,9 @@ class ProductController extends Controller
         else{
             return redirect('/login');
         }
+    }
+    static function cartItem(){ // static function cz when we dont use it we have a problem of non static methode to be called statically
+        $userId=Session::get('user')['id'];
+        return Cart::where('user_id',$userId)->count(); // count the items in our table 'cart' in DB to show the number of items in the header
     }
 }
--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,3 +1,8 @@
+ <?php
+  use App\Http\Controllers\ProductController;
+  $total=ProductController::cartItem();
+?> 
+
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <a class="navbar-brand" href="#">Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -19,7 +24,7 @@
       <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
     </form>
     <li class="nav-item">
-        <a class="nav-link" href="#">Cart(0)</a>
+        <a class="nav-link" href="#">Cart({{$total}})</a>
       </li>
     </ul>
     
